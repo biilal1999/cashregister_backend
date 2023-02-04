75f3494f6b1a80146d1cbb2a1b7004cc3a5eab45
Bilal Chekfeh <bilal99@correo.ugr.es>
2023-02-04 19:30:41 +0100

    CR-05 Write code in order to pass the tests

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -5,11 +5,18 @@ class CartProduct < ApplicationRecord
     validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
     validates :cart_id, uniqueness: { scope: :product_id, message: "The current cart has already this product" }
 
+    after_destroy :check_empty_cart
+
     def product_code
         product&.product_code
     end
 
     def product_price
         product&.price
+    end
+
+    def check_empty_cart
+        cart_product = CartProduct.where(cart_id: self.cart_id).first
+        Cart.find_by_id(self.cart_id)&.destroy if cart_product.blank?
     end
 end
