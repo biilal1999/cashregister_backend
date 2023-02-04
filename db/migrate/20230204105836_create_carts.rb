1629a4e615a805d9587ec237ba1f742129d41b01
Bilal Chekfeh <bilal99@correo.ugr.es>
2023-02-04 16:16:01 +0100

    Add tests for creating and fetching products as well as fix databases issues

--- db/migrate/20230204105836_create_carts.rb
+++ db/migrate/20230204105836_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
-      t.string :status, null: false
+      t.string :status, default: 'PENDING', null: false
       t.float :total_price, default: 0.0, null: false
       t.timestamps
     end
