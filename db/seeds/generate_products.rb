cbafe204b0c33ff6e0922a542a856dc242f5a9e6
Bilal Chekfeh <bilal99@correo.ugr.es>
2023-02-08 18:39:27 +0100

    Fix seeds error

--- db/seeds/generate_products.rb
+++ db/seeds/generate_products.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-Product.where(product_code: 'GR1', name: 'Green Tea').first_or_create!
-Product.where(product_code: 'SR1', name: 'Strawberries').first_or_create!
-Product.where(product_code: 'CF1', name: 'Coffee').first_or_create!
+product = Product.where(product_code: 'GR1', name: 'Green Tea').first
+Product.create(product_code: 'GR1', name: 'Green Tea', price: 3.11) unless product
+
+product = Product.where(product_code: 'SR1', name: 'Strawberries').first
+Product.create(product_code: 'SR1', name: 'Strawberries', price: 5) unless product
+
+product = Product.where(product_code: 'CF1', name: 'Coffee').first
+Product.create(product_code: 'CF1', name: 'Coffee', price: 11.23) unless product
